ef71deb5e9994febe4067a025d521b20db533174
Diego Matricardi <diegobmatricardi@hotmail.com>
2025-03-27 12:25:51 -0300

    OrderStatus enum

--- src/main/java/com/projWeb/course/Entities/Order.java
+++ src/main/java/com/projWeb/course/Entities/Order.java
@@ -1,6 +1,7 @@
 package com.projWeb.course.Entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.projWeb.course.Entities.Enum.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,6 +18,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-mm-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne /// generate association between customer and order at the database
     @JoinColumn(name = "client_id")/// creating the foreign key
     private User client;
@@ -24,9 +27,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -56,6 +60,17 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+
     }
 
     public User getClient() {
