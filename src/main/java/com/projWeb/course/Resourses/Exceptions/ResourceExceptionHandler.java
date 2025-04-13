4ba695511fcd41dae2ed4251354d0dacf7385f55
Diego Matricardi <diegobmatricardi@hotmail.com>
2025-04-13 20:05:23 -0300

    Exception handling - delete

--- src/main/java/com/projWeb/course/Resourses/Exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projWeb/course/Resourses/Exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.projWeb.course.Resourses.Exceptions;
 
+import com.projWeb.course.Services.Exceptions.DatabaseException;
 import com.projWeb.course.Services.Exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,14 @@ public class ResourceExceptionHandler {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(),status.value(),error, ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> resourceNotFound(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(),status.value(),error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
 }
--- src/main/java/com/projWeb/course/Services/UserService.java
+++ src/main/java/com/projWeb/course/Services/UserService.java
@@ -2,8 +2,11 @@ package com.projWeb.course.Services;
 
 import com.projWeb.course.Entities.User;
 import com.projWeb.course.Repository.UserRepository;
+import com.projWeb.course.Services.Exceptions.DatabaseException;
 import com.projWeb.course.Services.Exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -29,8 +32,17 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        if (!userRepository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+
+        try {
+            userRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
+
 
     public User update(Long id, User obj){
         User user = userRepository.getReferenceById(id);
